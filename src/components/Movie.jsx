3e55f953b53e4aab2ef56e1431b048e04a89b8cb
muhammad iqbal <gilangmohikbal@gmail.com>
2024-01-11 22:26:38 +0700

    detele file

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,30 +1,4 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { addListMovie } from "../redux/getMovie";
-// import { useEffect, useState } from "react";
-
 export default function Movie(props) {
-  //   const [movie, setMovies] = useState([]);
-  //   const dispatch = useDispatch();
-  //   const listMovie = useSelector((state) => state.movie.items);
-  //   console.log(listMovie);
-
-  //   useEffect(() => {
-  //     const fetchMovie = async () => {
-  //       try {
-  //         const response = await fetch(
-  //           `https://www.omdbapi.com/?apikey=a988d0e1&s=${props.dataMovie}`
-  //         );
-  //         const data = await response.json();
-  //         setMovies(data.Search);
-
-  //         dispatch(addListMovie(data));
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     };
-  //     fetchMovie();
-  //   }, [search]);
-
   return (
     <div className="container w-100 h-100 d-flex flex-wrap">
       {props.dataMovie?.map((movie, i) => {
